5b585c9b4cca49a0f812b2267e6949d174892157
shch989 <tankpop11@gmail.com>
2022-09-29 23:59:42 +0900

    Get 방식 연습

---
+++ src/main/java/com/example/demo/dto/UserRequest.java
@@ -0,0 +1,41 @@
+package com.example.demo.dto;
+
+public class UserRequest {
+
+    private String name;
+    private String email;
+    private String age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "UserRequest{" +
+                "name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", age='" + age + '\'' +
+                '}';
+    }
+}
